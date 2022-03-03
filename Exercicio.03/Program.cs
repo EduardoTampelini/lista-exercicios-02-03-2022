01ed5b5f757d8d0177950d1732afc3d3494267b1
Eduardo <ezafalon90@gmail.com>
2022-03-02 21:59:05 -0300

    exercicios 03-04-05

--- Exercicio.03/Program.cs
+++ Exercicio.03/Program.cs
@@ -6,7 +6,21 @@ namespace Exercicio._03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Informe a distancia percorrida: ");
+            string valorDiastancia = Console.ReadLine();
+            decimal distanciaConvertida = 0.0m;
+
+            Console.WriteLine("Informe o consumo percorrida: ");
+            string valorConsumo = Console.ReadLine();
+            decimal consumoConvertido = 0.0m;
+
+            decimal.TryParse(valorDiastancia, out distanciaConvertida);
+            decimal.TryParse(valorConsumo, out consumoConvertido);
+
+            decimal valorMedio = distanciaConvertida / consumoConvertido;
+
+            Console.WriteLine($"O calculo da media é : {valorMedio}");
+            Console.ReadLine();
         }
     }
 }
---
+++ Exercicio.05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o primeiro valor para calculo");
+
+            string primeiroValor = Console.ReadLine();
+            decimal valor1Convertido = 0.0m;
+
+            decimal.TryParse(primeiroValor, out valor1Convertido);
+
+            Console.WriteLine("Informe o segundo valor para o calculo");
+
+            string segundoValor = Console.ReadLine();
+            decimal valor2Convertido = 0.0m;
+
+            decimal.TryParse(segundoValor, out valor2Convertido);
+
+            decimal soma = valor1Convertido + valor2Convertido;
+            Console.WriteLine($"Resultado da soma: {soma} ");
+
+            decimal subtracao = valor1Convertido - valor2Convertido;
+            Console.WriteLine($"Resultado da subtração: {subtracao} ");
+
+            decimal multiplicacao = valor1Convertido * valor2Convertido;
+            Console.WriteLine($"Resultado da multiplicação: {multiplicacao} ");
+
+            decimal divisao = valor1Convertido / valor2Convertido;
+            Console.WriteLine($"Resultado da divisao: {divisao} ");
+
+            decimal resultFinal = valor1Convertido % valor2Convertido;
+
+            Console.WriteLine($"Resultado da divisao: {resultFinal} ");
+
+            Console.ReadLine();
+        }
+    }
+}
