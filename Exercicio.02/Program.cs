720e5aec25acfe52a07a101c3164845c769c01b3
Eduardo <ezafalon90@gmail.com>
2022-03-02 21:05:31 -0300

    exercicios

---
+++ Exercicio.02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor em Dolar:");
+            string valorDigitado = Console.ReadLine();
+            decimal valorConvertido = 0.0m;
+            decimal valorCambio = 5.22m;
+
+            decimal.TryParse(valorDigitado, out valorConvertido);
+
+            decimal valorCambioConvertido = valorConvertido * valorCambio;
+
+            Console.WriteLine($"valor em R${valorCambioConvertido}");
+            Console.ReadLine();
+        }
+    }
+}
