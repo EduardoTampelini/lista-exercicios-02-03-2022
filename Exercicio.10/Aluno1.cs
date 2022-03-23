692d08d8ac5a5b38d6232a4fbefa6b5a4f6be19b
eduardo <ezafalon90@gmail.com>
2022-03-23 20:25:05 -0300

    Exercicio 10

---
+++ Exercicio.10/Aluno1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio._10
+{
+    class Aluno1 : AbstractAluno
+    {
+        public override int ObterFalta()
+        {
+            return 8;
+        }
+
+        public override string ObterNome()
+        {
+            return "Eduardo";
+        }
+
+        public override int ObterNota()
+        {
+           return 90;
+        }
+
+        public override string ObterRa()
+        {
+            return "661";
+        }
+    }
+}
